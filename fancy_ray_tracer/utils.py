a90fceb78f404bc8b3141b085db84d745ec10b13
Frank S <fsadannn@gmail.com>
2021-12-24 13:07:55 -0500

    update

--- fancy_ray_tracer/utils.py
+++ fancy_ray_tracer/utils.py
@@ -3,14 +3,15 @@ from typing import List
 
 import numpy as np
 
-# def chain(ops: List[np.ndarray]) -> np.ndarray:
-#     if len(ops) == 0:
-#         return np.eye(4, 4)
-#     res: np.ndarray = ops[0]
-#     for op in ops[1:]:
-#         res = op.dot(res)
 
-#     return res
+def chain_ops(ops: List[np.ndarray]) -> np.ndarray:
+    if len(ops) == 0:
+        return np.eye(4, 4)
+    res: np.ndarray = ops[0]
+    for op in ops[1:]:
+        res = op.dot(res)
+
+    return res
 
 
 def chain(ops: List[np.ndarray], p: np.ndarray) -> np.ndarray:
